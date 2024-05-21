1973b39991c9bf55a6121da3e1b5068ce10f14d0
Chwoeger Charleston <122495108+shwaygrr@users.noreply.github.com>
2024-05-21 17:01:45 -0400

    integrate into function and assess bug fix

--- node-sweeper/node-sweeper.ps1
+++ node-sweeper/node-sweeper.ps1
@@ -1,6 +1,7 @@
 function Get-DirectorySize {
+    [CmdletBinding()]
     param(
-        [String]$Path
+        [string]$Path
     )
     $totalSizeBytes = (Get-ChildItem -Path $path -Recurse -File | Measure-Object -Property length -Sum).Sum
     $totalSizeKB = [math]::Round($totalSizeBytes / 1KB, 2) #convert to KB
@@ -8,45 +9,54 @@ function Get-DirectorySize {
     return $totalSizeKB
 }
 
-#path of folder to search for node modules
-$path = "C:\Users\15616\OneDrive\Documents\Fall 2023\Prin. of SWE\hw3"
+function Remove-NodeModules {
+    [CmdletBinding()]
+    param (
+        [string]$Path
+    )
 
-#get assessment decision
-$wantAssessment = Read-Host -Prompt "Would like an assessment before deletion? (Y/N) Default is No" 
+    #get assessment decision
+    $wantAssessment = Read-Host -Prompt "Would like an assessment before deletion? (Y/N) Default is No" 
 
-#search and delete
-Switch ($wantAssessment) {
-    "Y" {
-        $modulesArr = Get-ChildItem -Path $path -Filter "node_modules" -Recurse -Directory | 
-            Where-Object { $_.Parent.FullName -notmatch "\\node_modules" }
+    #search and delete
+    Switch ($wantAssessment) {
+        "Y" {
+            $modulesArr = Get-ChildItem -Path $Path -Filter "node_modules" -Recurse -Directory | 
+                Where-Object { $_.Parent.FullName -notmatch "\\node_modules" }
 
-        #assessment
-        $parentSize = Get-DirectorySize -Path $path
-        $modulesSize = 0
+            #assessment            
+            Write-Host ("There are {0} node_modules folders in the folder" -f $modulesArr.Length)
 
-        Foreach ($module in $modulesArr) {
-            $modulesSize += Get-DirectorySize -Path $module.FullName 
+            $parentSize = Get-DirectorySize -Path $Path
+            If ($parentSize -ne 0) {
+                $modulesSize = 0
+                Foreach ($module in $modulesArr) {
+                    $modulesSize += Get-DirectorySize -Path $module.FullName 
+                }
+                Write-Host ("Node modules folders are taking up {0}% of parent folder" -f (($modulesSize / $parentSize) * 100))
+            } Else {
+                Write-Host "Parent Folder is 0 KB"
+            }
+
+            $readyDelete = Read-Host -Prompt "Ready to delete?(Y/N) Default is Yes"
+            
+            If ($readyDelete -ne "N") {
+                $modulesArr | ForEach-Object { Remove-Item -Path $_.FullName -Force -Recurse }
+                Write-Host "Deletions complete"
+            }
+
+            break
         }
 
-        Write-Host ("There are {0} node_modules folders in the {1} folder" -f $modulesArr.Length, $path)
-        Write-Host ("node_module folders are taking up {0}% of {1} folder space" -f ($modulesSize / $parentSize * 100), $path)
-
-        $readyDelete = Read-Host -Prompt "Ready to delete?(Y/N) Default is Yes"
-        
-        If ($readyDelete -ne "N") {
-            $modulesArr | ForEach-Object { Remove-Item -Path $_.FullName -Force -Recurse }
+        default {
+            Get-ChildItem -Path $path -Filter "node_modules" -Recurse -Directory | 
+                Where-Object { $_.Parent.FullName -notmatch "\\node_modules" } |
+                Foreach-Object { Remove-Item -Path $_.FullName -Force -Recurse }
+            
             Write-Host "Deletions complete"
+            break
         }
-
-        break
-    }
-
-    default {
-        Get-ChildItem -Path $path -Filter "node_modules" -Recurse -Directory | 
-            Where-Object { $_.Parent.FullName -notmatch "\\node_modules" } |
-            Foreach-Object { Remove-Item -Path $_.FullName -Force -Recurse }
-        
-        Write-Host "Deletions complete"
-        break
     }
 }
+
+Remove-NodeModules
